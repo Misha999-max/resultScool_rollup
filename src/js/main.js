8f779591e569114d5dbbe427e31e6164b49a6714
__BEAST__ <mihailkalacev9@gmail.com>
2023-01-07 12:38:03 +0300

    add foto

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 import '../css/style.css'
+import image from '../../assets/foto-had.jpg';
 
-
+const img = document.querySelector('.img_logo')
+img.src = image
+console.log(img)
 console.log('hello world')
